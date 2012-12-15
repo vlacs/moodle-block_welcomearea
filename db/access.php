aca7948b95bd1e073de561b11d63d29b53d160df
Karen Eng <keng@vlacs.org>
2012-12-14 22:03:19 -0500

    Lots of updates to make compatible with moodle 2.3

---
+++ db/access.php
@@ -0,0 +1,31 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = array(
+
+    'block/welcomearea:manageownarea' => array(
+
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => array(
+            'teacher' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        )
+    ),
+
+    'block/welcomearea:managedefault' => array(
+
+        'riskbitmask' => RISK_SPAM,
+
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => array(
+            'manager' => CAP_ALLOW
+        )
+    )
+
+);
+
+
